bff4104651d728458d30307c0189e4202a91798e
eunbiggabi <eunbiggabi84@gmail.com>
2022-01-19 20:38:58 +1000

    add photo for home page

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,10 +1,25 @@
-import { HomeSection, HomeContainer, HomeTitle } from "./home.styles";
+import {
+  HomeSection,
+  HomeContainer,
+  HomeMainContainer,
+  HomeInfo,
+  HomeTitle,
+  HomeDescription,
+  HomeButton,
+  HomeMainImage,
+  HomeSide,
+  HomeSideImage1,
+  HomeSideImage2,
+  HomeSideTitle,
+  HomeSideDesciption
+} from "./home.styles";
+import MainImage from '../../../assets/images/home/profile.jpeg'
 
 export default function Home() {
   return (
     <HomeSection>
       <HomeContainer>
-        <HomeMain>
+        <HomeMainContainer>
           <HomeInfo>
             <HomeTitle>I am Chloe, sharing my daily photo here!</HomeTitle>
             <HomeDescription>
@@ -15,11 +30,12 @@ export default function Home() {
             </HomeDescription>
             <HomeButton />
           </HomeInfo>
-          <HomeImage />
-        </HomeMain>
+          <HomeMainImage src={MainImage} />
+        </HomeMainContainer>
         <HomeSide>
-          <HomeSideImage1 />
-          <HomeSideImage2 />
+          <HomeSideImage1 src="images/home/homeSide1.jpeg" />
+          <HomeSideImage2 src="images/home/homeSide2.jpeg" />
+          <HomeSideTitle>This is Side title</HomeSideTitle>
           <HomeSideDesciption>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
             deleniti debitis, fuga, eum nisi harum, nobis suscipit nulla est nam
--- src/components/pages/home/home.styles.js
+++ src/components/pages/home/home.styles.js
@@ -6,13 +6,51 @@ export const HomeSection = styled.section`
   justify-content: center;
   align-items: center;
   font-size: 1.2rem;
+  background: #101522;
+  color: #fff;
 `;
 
 export const HomeContainer = styled(Container)`
   ${Container}
 `;
 
+export const HomeMainContainer = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 760px) {
+    flex-direction: column;
+  }
+`;
+
+export const HomeInfo = styled.div``;
+
 export const HomeTitle = styled.h1`
-  color: black;
   text-align: center;
 `;
+
+export const HomeDescription = styled.p``;
+
+export const HomeButton = styled.button``;
+
+export const HomeMainImage = styled.img`
+  width: 480px;
+  height: 480px;
+  border-radius: 0.5rem;
+  transform: rotate(10deg);
+
+  @media screen and (max-width: 760px) {
+    width: 240px;
+    height: 240px;
+    transform: none;
+  }
+`;
+
+export const HomeSide = styled.div``;
+
+export const HomeSideImage1 = styled.img``;
+
+export const HomeSideImage2 = styled.img``;
+
+export const HomeSideTitle = styled.h2``;
+
+export const HomeSideDesciption = styled.p``;
