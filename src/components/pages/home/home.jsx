315a167ef2b6378ed4514dcdaa8c5e5729ab6136
eunbiggabi <eunbiggabi84@gmail.com>
2022-04-01 10:49:53 +1000

    add readMore

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   HomeSection,
   HomeContainer,
@@ -11,14 +12,20 @@ import {
   HomeSideImage1,
   HomeSideImage2,
   HomeSideImage3,
-} from "./home.styles";
-import MainImage from "../../../assets/images/home/profile.jpeg";
-import SideImage1 from "../../../assets/images/home/homeSide1.jpeg";
-import SideImage2 from "../../../assets/images/home/homeSide2.jpeg";
-import SideImage3 from "../../../assets/images/home/homeSide3.jpeg";
-import { Button } from "../../button/button.styles";
+} from './home.styles';
+import MainImage from '../../../assets/images/home/profile.jpeg';
+import SideImage1 from '../../../assets/images/home/homeSide1.jpeg';
+import SideImage2 from '../../../assets/images/home/homeSide2.jpeg';
+import SideImage3 from '../../../assets/images/home/homeSide3.jpeg';
+import { Button } from '../../button/button.styles';
 
 export default function Home() {
+  const [readMore, setReadMore] = useState(false);
+  const homeDescription = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+  cumque, officia adipisci veniam iusto animi! Magni doloremque,
+  dicta quod beatae labore nemo molestias distinctio facere ex cum
+  est cumque! Distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cumque ipsum quia soluta eveniet, libero adipisci molestias quam vero earum hic, tempora error qui minus, ut consectetur reprehenderit accusamus accusantium? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni nisi hic doloribus voluptas, non assumenda mollitia explicabo est. Esse ea perferendis neque minus pariatur quam temporibus eum dolor incidunt?`;
+
   return (
     <HomeSection>
       <HomeContainer>
@@ -26,10 +33,12 @@ export default function Home() {
           <HomeInfo>
             <HomeTitle>I am Chloe, sharing my daily photo here!</HomeTitle>
             <HomeDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              cumque, officia adipisci veniam iusto animi! Magni doloremque,
-              dicta quod beatae labore nemo molestias distinctio facere ex cum
-              est cumque! Distinctio.
+              {readMore
+                ? homeDescription
+                : `${homeDescription.substring(0, 100)}...`}
+              <button onClick={() => setReadMore(!readMore)}>
+                {readMore ? 'Show Less' : 'Read More'}
+              </button>
             </HomeDescription>
             <Button bg="red" />
           </HomeInfo>
